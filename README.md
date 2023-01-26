90887aded77987ec3bed5d13f64115e5273a10fb
abdelrahman167565 <abdelrahman.amer1999@gmail.com>
2023-01-26 22:52:01 +0200

    landing page uploading and fixing errors

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
-# Landing Page Project
+<h1>Introduction</h1>
+<h3>Landing Page Project</h3>
+<p>This is an educational project submitted to Udacity through EgFWD </p>
+<h1>Project Overview</h1>
+<p>Creating a landing page using various JavaScript functions</p>
+<p> First of all I created global variables to hold the section and the navigation bar list</p>
+<p>second I began my work by buildnav function that takes sections and navlist as parameter, I started a for each loop on sections with element as argument to loop on all the sections we have and for every section, an anchor tag is added to hold the menu__link class and linking each list item to the appropriate section using href element.id to get the actual section id and the text, and called build nav function to begin.
+<p>after that I add an event listener  to do multiple things</p> 
+<p>1.decide when the scroll up button appear</p> 
+<p>2.added another for each section to get the view rectangle and decide which section is in view port and adding your-active-class to this section</p> 
+<p>3. when the section is no longer in view the your-active-class is removed.</p>
 
-## Table of Contents
+<h1>Technologies used</h1>
 
-* [Instructions](#instructions)
+![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)
+![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)
 
-## Instructions
-
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
-
-To get started, open `js/app.js` and start building out the app's functionality
-
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+<h1>References</h1>
--- js/app.js
+++ js/app.js
@@ -1,62 +1,87 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
+ *
+ */
 
 /**
  * Comments should be present at the beginning of each procedure and class.
  * Great to have comments before crucial code sections within the procedure.
-*/
+ */
 
 /**
  * Define Global Variables
- * 
-*/
-
+ *
+ */
+const sections = document.querySelectorAll("section");
+const navList = document.getElementById("navbar__list");
+let lastActiveSec = null;
+const activeSec = "active";
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
-*/
+ *
+ */
+const buildnav = (sections, navList) => {
+  sections.forEach((element) => {
+    let newlist = document.createElement("li");
+    newlist.innerHTML = `<a class="menu__link"  data-nav='${element.id}' href="#${element.id}"> ${element.dataset.nav} </a>`;
+    navList.append(newlist);
+  });
+
+  //adding an event listener on click calling function setActiveSecNav--- to set the active section on navigation bar
+  navList.addEventListener("click", setActiveSecNav);
+};
+
+//another Event listener on click to scroll *SMOOTHLY* TO THE REQUIRED SECTION
+navList.addEventListener("click", (ele) => {
+  ele.preventDefault();
+  console.log(ele.target.dataset.nav);
+  if (ele.target.dataset.nav) {
+    console.log("ele.target.dataset.nav");
+    document
+      .getElementById(`${ele.target.dataset.nav}`)
+      .scrollIntoView({ behavior: "smooth" });
+  }
+});
+
+//THE FUNCTION CALLED IN BUILDNAV() TO ADD/REMOVE ACTIVE FROM SECTIONS AND THUS HIGHLITING THEM
+const setActiveSecNav = (elm) => {
+  elm.target.classList.add(activeSec);
+  if (lastActiveSec !== null) {
+    lastActiveSec.classList.remove(activeSec);
+  }
+  lastActiveSec = elm.target;
+};
+
+// EVENT LISTNENR ON SCROLL TO SET THE SECTION IN VIEW AS ACTIVE & MAKE THE SCROLLUP BUTTON APPEAR TO DISAPPEAR
+window.addEventListener("scroll", (scrollevent) => {
+  if (document.body.scrollTop > 250) {
+    document.getElementById("scrollUpButton").style.display = "block";
+  } else {
+    document.getElementById("scrollUpButton").style.display = "none";
+  }
+  sections.forEach((element) => {
+    const top = element.getBoundingClientRect().top;
+    if (top >= -100 && top <= 200) {
+      element.classList.add("your-active-class");
+    } else {
+      element.classList.remove("your-active-class");
+    }
+  });
+});
 
 
 
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
+// function called to Build menu (The start)
+buildnav(sections, navList);
